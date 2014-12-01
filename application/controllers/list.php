9a1c407048d511d1f24eb6f939d14cffd9ffef3e
Phan Khắc Đạo <daofresh@live.com>
2014-12-02 05:41:04 +0700

    Add example controller with build-in php name
    class _List extends TM_Controller

---
+++ application/controllers/list.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* List Controller
+* Why controller name is _List? Because list is build-in php name
+**/
+class _List extends TM_Controller
+{
+	public function index()
+	{
+		echo "This is list controller";
+	}
+}
