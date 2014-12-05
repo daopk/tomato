8eefa7e7597bb0fd9963933b801dbc17b6f5a254
Phan Khắc Đạo <daofresh@live.com>
2014-12-05 17:26:41 +0700

    Remove bootstrap theme

--- application/template/default/config/BundleConfig.php
+++ application/template/default/config/BundleConfig.php
@@ -1,15 +1,15 @@
 <?php
 
 Template::AddStyle(array(
-	"bootstrap" => array("bootstrap.min.css", "bootstrap-theme.min.css"),
+	"bootstrap" => array("bootstrap.min.css"),
 	"style" => array("tomato.css"),
 	));
 
 
 
 Template::AddScript(array(
-	"jquery" => array("jquery-1.11.1.min.js"), 
-	"bootstrap" => array("bootstrap.min.js"),
+	//"jquery" => array("jquery-1.11.1.min.js"), 
+	//"bootstrap" => array("bootstrap.min.js"),
 	"AutoPrefixer" => array("prefixfree.min.js")
 	));
 ?>
