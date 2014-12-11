ec55138c3cdb3adb881462f515c83996b289376e
Phan Khắc Đạo <daofresh@live.com>
2014-12-11 16:10:13 +0700

    Make database config is optional

--- application/core/libs/database/database.php
+++ application/core/libs/database/database.php
@@ -1,17 +1,19 @@
 <?php
 
-require_once 'NotORM.php';
+$dbconfig = JsonConfig::GetConfigByName('database');
+if($dbconfig != null)
+{
+	require_once 'NotORM.php';
+	extract(JsonConfig::GetConfigByName('database'));
 
-extract(JsonConfig::GetConfigByName('database'));
+	$options = array(
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+		); 
 
-$options = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-); 
-
-try {
-	$connection = new PDO('mysql:host='.$host.';dbname='.$dbname, $username, $password, $options);
-	$db = new NotORM($connection);	
-} catch (Exception $e) {
-	throw new DatabaseException("Can't connect database. Check your config in application/configuration/config.json");
-	
+	try {
+		$connection = new PDO('mysql:host='.$host.';dbname='.$dbname, $username, $password, $options);
+		$db = new NotORM($connection);	
+	} catch (Exception $e) {
+		throw new DatabaseException("Can't connect database. Check your config in application/configuration/config.json");	
+	}
 }
