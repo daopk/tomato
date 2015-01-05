c579adbbe75c218fec8d9629859e51f3586f86be
Phan Khắc Đạo <1112065@student.hcmus.edu.vn>
2015-01-05 08:14:18 +0700

    Add comment

--- application/core/base/model.php
+++ application/core/base/model.php
@@ -1,12 +1,30 @@
 <?php
 
 /**
-* 
-*/
+ * TM_Model class
+ */
 class TM_Model
 {
+	var $db;
+
 	function __construct()
 	{
-		# code...
+		$config = _Tomato::$config->database;
+		if(isset($config))
+			$this->db = $db = new NotORM($this->ConnectDB($config));
+	}
+
+	private function ConnectDB($config)
+	{
+
+		switch ($config->driver) {
+			case 'mysql':
+				$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+				return new PDO('mysql:host='.$config->host.';dbname='.$config->db_name, $config->user, $config->pass, $options);
+				break;
+			case 'sqlite':
+				return new PDO('sqlite:'.APP_DIR.$config->file);
+				break;
+		}
 	}
 }
