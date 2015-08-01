0fdea7ccf3278f14685eac11e5a1a676129ab450
Phan Khac Dao <daofresh@gmail.com>
2015-08-01 14:44:10 +0700

    Change front template

--- template/front/index.php
+++ template/front/index.php
@@ -7,7 +7,29 @@
     <?php $this->RenderStyle() ?>
 </head>
 <body>
-    <?php $this->RenderBody() ?>
+    <nav class="orange darken-4" role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Tomato</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+    <div class="container row section">
+        <?php $this->RenderBody() ?>
+    </div>
+
+    <footer class="page-footer">
+        <div class="footer-copyright">
+            <div class="container">
+                © 2014-2015 Materialize, All rights reserved.
+            </div>
+        </div>
+    </footer>
     <?php $this->RenderScript() ?>
 </body>
 </html>
